8d651be0a5e5c284ae6d3e6c2e4840ed4e374082
riocastillo <89022215+riocastillo@users.noreply.github.com>
2021-10-17 17:27:50 -0400

    initial commit

---
+++ public/main.js
@@ -0,0 +1,72 @@
+var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[5].innerText.trim()
+        const likes = Number(this.parentNode.parentNode.childNodes[7].innerText)
+        console.log("name" + name, msg, likes)
+        fetch('increment', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'likes':likes
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[5].innerText.trim()
+    const likes = Number(this.parentNode.parentNode.childNodes[7].innerText)
+    console.log(likes)
+    fetch('decrease', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'likes':likes
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[5].innerText.trim()
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
